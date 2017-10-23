eab06984c6bd80ee9b0c7c88805f9b1f87e6c589
Pietro Rea <pietro@sweetpeamobile.com>
2017-10-22 22:25:14 -0400

    Renaming MainActivityKotlin -> MainActivity

--- kotlin/AllTheRecipes/app/src/main/java/com/raywenderlich/alltherecipes/MainActivity.kt
+++ kotlin/AllTheRecipes/app/src/main/java/com/raywenderlich/alltherecipes/MainActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 
-class MainActivityKotlin : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private var listView: ListView? = null
 
